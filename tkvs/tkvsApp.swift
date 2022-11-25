1535780e1b1b2499b8d13b117a911b769aaea017
ocelotspleen <ocelotspleen@users.noreply.github.com>
2022-11-25 16:07:22 +0000

    Initial Commit

---
+++ tkvs/tkvsApp.swift
@@ -0,0 +1,17 @@
+//
+//  tkvsApp.swift
+//  tkvs
+//
+//  Created by Stephen Parker on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct tkvsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
